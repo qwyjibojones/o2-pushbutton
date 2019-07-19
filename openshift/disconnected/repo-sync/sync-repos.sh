c3090767e86e8d9c2670871238ee3d5ad17d8728
Garrett Potts <gpotts@radiantblue.com>
2019-07-19 14:24:20 -0400

    Adding IPsec prereuisites to the build of RPMS

--- openshift/disconnected/repo-sync/sync-repos.sh
+++ openshift/disconnected/repo-sync/sync-repos.sh
@@ -13,6 +13,16 @@ fi
 mkdir -p $DESTINATION_DIR
 
 pushd $DESTINATION_DIR
+mkdir -p rpmbuild/SOURCES
+wget -q http://openvswitch.org/releases/openvswitch-2.9.2.tar.gz -O openvswitch-2.9.2.tar.gz
+cp openvswitch-2.9.2.tar.gz rpmbuild/SOURCES/
+tar xfz openvswitch-2.9.2.tar.gz;
+$SCRIPT_DIR/docker-run.sh os-sync "rpmbuild -bb --nocheck openvswitch-2.9.2/rhel/openvswitch-fedora.spec"
+mkdir extras
+mv rpmbuild/RPMS/* extras/
+rm -rf rpmbuild
+rm -f openvswitch*
 $SCRIPT_DIR/docker-run.sh os-sync "reposync -n -r centos-openshift-origin311"
 $SCRIPT_DIR/docker-run.sh os-sync "createrepo --simple-md-filenames centos-openshift-origin311"
+$SCRIPT_DIR/docker-run.sh os-sync "createrepo --simple-md-filenames extras"
 popd
