aaaef91bf70668f1d9f62a3155dc8fcd63a45d55
Garrett Potts <gpotts@radiantblue.com>
2019-08-28 10:49:58 -0400

    Added if checks

--- openshift/disconnected/containers/bundle-okd-311-images.sh
+++ openshift/disconnected/containers/bundle-okd-311-images.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+pushd `dirname ${BASH_SOURCE[0]}` >/dev/null
+BUNDLE_OKD_SCRIPT_DIR=`pwd -P`
+popd >/dev/null
 SOURCE_DIR=$1
 
-if [ "$SOURCE_DIR" == "" ] ; then
-   SOURCE_DIR="/data/docker-registry-data"
+if [ "${SOURCE_DIR}" == "" ] ; then
+   echo "Please specify the location of your docker registry root directory...."
+   exit 1  
 fi
-pushd /data/
-tar cvfz docker-registry-data.tgz docker-registry-data
+pushd ${SOURCE_DIR}
+tar cvfz docker-registry-data.tgz registry
 popd
